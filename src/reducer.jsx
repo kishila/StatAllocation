80c7fa6b4ab42437c66bcb94997daa56fd37b5e9
MaxMEllon <maxmellon1994@gmail.com>
2017-01-25 19:22:33 +0900

    :fire: Remove node_module

--- src/reducer.jsx
+++ src/reducer.jsx
@@ -38,49 +38,53 @@ const initialState = {
 
 export default function reducer(state = initialState, action) {
   // 選択された素性のデータを取得
-   var setSelectedClassValue = function(selectedClass){
-     var selClassData = jsonData[selectedClass];
-     return {
-       "level":selClassData.level,
-       "vigor":selClassData.vigor,
-       "attunement":selClassData.attunment,
-       "endurance":selClassData.endurance,
-       "vitality":selClassData.vitality,
-       "strength":selClassData.strength,
-       "dexterity":selClassData.dexterity,
-       "intelligence":selClassData.intelligence,
-       "faith":selClassData.faith,
-       "luck":selClassData.luck
-     };
-   }
+  var setSelectedClassValue = function(selectedClass){
+    var selClassData = jsonData[selectedClass];
+    return {
+      "level":selClassData.level,
+      "vigor":selClassData.vigor,
+      "attunement":selClassData.attunment,
+      "endurance":selClassData.endurance,
+      "vitality":selClassData.vitality,
+      "strength":selClassData.strength,
+      "dexterity":selClassData.dexterity,
+      "intelligence":selClassData.intelligence,
+      "faith":selClassData.faith,
+      "luck":selClassData.luck
+    };
+  }
 
-   //  ステータスの更新
-   var updateStatus = function(tmpClassValue){
-     return {
-       "level":tmpClassValue.level,
-       "vigor":tmpClassValue.vigor,
-       "attunement":tmpClassValue.attunment,
-       "endurance":tmpClassValue.endurance,
-       "vitality":tmpClassValue.vitality,
-       "strength":tmpClassValue.strength,
-       "dexterity":tmpClassValue.dexterity,
-       "intelligence":tmpClassValue.intelligence,
-       "faith":tmpClassValue.faith,
-       "luck":tmpClassValue.luck
-     }
-   }
+  //  ステータスの更新
+  var updateStatus = function(tmpClassValue){
+    return {
+      "level":tmpClassValue.level,
+      "vigor":tmpClassValue.vigor,
+      "attunement":tmpClassValue.attunment,
+      "endurance":tmpClassValue.endurance,
+      "vitality":tmpClassValue.vitality,
+      "strength":tmpClassValue.strength,
+      "dexterity":tmpClassValue.dexterity,
+      "intelligence":tmpClassValue.intelligence,
+      "faith":tmpClassValue.faith,
+      "luck":tmpClassValue.luck
+    }
+  }
+
+  const updateReducer = (selectedClass, nextClass, nextStatus) => {
+    return Object.assign({}, state, {
+      selectedClass: selectedClass,
+      selectedClassValue: tmpClassValue,
+      status: tmpStatus
+    }) 
+  }
 
   switch(action.type) {
-    //- セレクターによる素性選択時のアクション
+      //- セレクターによる素性選択時のアクション
     case 'SELECT_KNIGHT_CLASS': {
       var selectedClass = "knight"
       var tmpClassValue = setSelectedClassValue(selectedClass);
       var tmpStatus = updateStatus(tmpClassValue);
-      return Object.assign({}, state, {
-        selectedClass: selectedClass,
-        selectedClassValue: tmpClassValue,
-        status: tmpStatus
-      });
+      return updateReducer(selectedClass, tmpClassValue, tmpStatus);
     }
     case 'SELECT_MERCENARY_CLASS': {
       var selectedClass = "mercenary"
