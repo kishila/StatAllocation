670a25ebc911ad65de7b540d646e49243c3f87bb
kishila <s16g459@stu.kagawa-u.ac.jp>
2017-01-25 13:15:24 +0900

    素性を選択時にアクションを実行

--- src/app.js
+++ src/app.js
@@ -5,23 +5,22 @@ import ReactDOM from 'react-dom'
 import reducer from './reducer.jsx'
 
 
-const Counter = ({
-  value1,
-  value2,
-  onIncrement1,
-  onDecrement1,
-  onIncrement2,
-  onDecrement2,
+const ClassesSelect = ({
+  selectedClass,
+  onChangeClass
 }) => (
-  <div>
-    <h1>{value1}</h1>
-    <button onClick={onIncrement1}>+</button>
-    <button onClick={onDecrement1}>-</button>
-    <br />
-    <h1>{value2}</h1>
-    <button onClick={onIncrement2}>+</button>
-    <button onClick={onDecrement2}>-</button>
-  </div>
+  <select onChange={onChangeClass}>
+    <option value="knight">騎士</option>
+    <option value="mercenary">傭兵</option>
+    <option value="warrior">戦士</option>
+    <option value="herald">伝令</option>
+    <option value="thief">盗人</option>
+    <option value="assassin">刺客</option>
+    <option value="sorcerer">魔術師</option>
+    <option value="pyromancer">呪術師</option>
+    <option value="cleric">聖職者</option>
+    <option value="deprived">持たざる者</option>
+  </select>
 )
 
 //作成したreducerであるcounter関数を引数に指定してstoreを作成
@@ -30,25 +29,16 @@ const store = createStore(reducer)
 //画面更新用の関数を作成
 const render = () => {
   ReactDOM.render(
-    <Counter
-      value1={store.getState().count1}
-      value2={store.getState().count2}
-    onIncrement1={() =>
-      store.dispatch({
-        type: 'INCREMENT1'
-      })}
-    onDecrement1={() =>
-      store.dispatch({
-        type: 'DECREMENT1'
-      })}
-    onIncrement2={() =>
-      store.dispatch({
-        type: 'INCREMENT2'
-      })}
-    onDecrement2={() =>
-      store.dispatch({
-        type: 'DECREMENT2'
-      })}
+    <ClassesSelect
+      selectedClass={store.getState().selectedClass}
+      onChangeClass={(e) =>
+        {
+          var action = "SELECT_" + e.target.value.toUpperCase() + "_CLASS";
+          store.dispatch({
+            type: action
+          })
+        }
+      }
     />,
     document.getElementById('root')
   )
