efea48067e56cee613ab2fc7ae644e7371b22486
kishila <s16g459@stu.kagawa-u.ac.jp>
2017-01-30 20:20:43 +0900

    ステ振り時のアクションを減らす

--- src/components/StatusViewer.js
+++ src/components/StatusViewer.js
@@ -5,15 +5,7 @@ const completeNum = (num) => {return (" " + num).slice(-2)}
 
 export default function StatusViewer({
   status, selectedClassValue, allocationValue,
-  onClickVigorDown, onClickVigorUp,
-  onClickAttunementDown, onClickAttunementUp,
-  onClickEnduranceDown, onClickEnduranceUp,
-  onClickVitalityDown, onClickVitalityUp,
-  onClickStrengthDown, onClickStrengthUp,
-  onClickDexterityDown, onClickDexterityUp,
-  onClickIntelligenceDown, onClickIntelligenceUp,
-  onClickFaithDown, onClickFaithUp,
-  onClickLuckDown, onClickLuckUp,
+  onClickAllocationDown, onClickAllocationUp,
   onClickReset
 }) {
   return (
@@ -26,73 +18,73 @@ export default function StatusViewer({
         <tr>
           <td>生命力</td>
           <td>
-            <span onClick={onClickVigorDown}>&lt;</span>
+            <span id="vigor" onClick={onClickAllocationDown}>&lt;</span>
             <span>{completeNum(status.vigor)}({completeNum(selectedClassValue.vigor)} + {completeNum(allocationValue.vigor)})</span>
-            <span onClick={onClickVigorUp}>&gt;</span>
+            <span id="vigor" onClick={onClickAllocationUp}>&gt;</span>
           </td>
         </tr>
         <tr>
           <td>集中力</td>
           <td>
-            <span onClick={onClickAttunementDown}>&lt;</span>
+            <span id="attunement" onClick={onClickAllocationDown}>&lt;</span>
             <span>{completeNum(status.attunement)}({completeNum(selectedClassValue.attunement)} + {completeNum(allocationValue.attunement)})</span>
-            <span onClick={onClickAttunementUp}>&gt;</span>
+            <span id="attunement" onClick={onClickAllocationUp}>&gt;</span>
           </td>
         </tr>
         <tr>
           <td>持久力</td>
           <td>
-            <span onClick={onClickEnduranceDown}>&lt;</span>
+            <span id="endurance" onClick={onClickAllocationDown}>&lt;</span>
             <span>{completeNum(status.endurance)}({completeNum(selectedClassValue.endurance)} + {completeNum(allocationValue.endurance)})</span>
-            <span onClick={onClickEnduranceUp}>&gt;</span>
+            <span id="endurance" onClick={onClickAllocationUp}>&gt;</span>
           </td>
         </tr>
         <tr>
           <td>体力</td>
           <td>
-            <span onClick={onClickVitalityDown}>&lt;</span>
+            <span id="vitality" onClick={onClickAllocationDown}>&lt;</span>
             <span>{completeNum(status.vitality)}( {completeNum(selectedClassValue.vitality)} + {completeNum(allocationValue.vitality)})</span>
-            <span onClick={onClickVitalityUp}>&gt;</span>
+            <span id="vitality" onClick={onClickAllocationUp}>&gt;</span>
           </td>
         </tr>
         <tr>
           <td>筋力</td>
           <td>
-            <span onClick={onClickStrengthDown}>&lt;</span>
+            <span id="strength" onClick={onClickAllocationDown}>&lt;</span>
             <span>{completeNum(status.strength)}({completeNum(selectedClassValue.strength)} + {completeNum(allocationValue.strength)})</span>
-            <span onClick={onClickStrengthUp}>&gt;</span>
+            <span id="strength" onClick={onClickAllocationUp}>&gt;</span>
           </td>
         </tr>
         <tr>
           <td>技量</td>
           <td>
-            <span onClick={onClickDexterityDown}>&lt;</span>
+            <span id="dexterity" onClick={onClickAllocationDown}>&lt;</span>
             <span>{completeNum(status.dexterity)}({completeNum(selectedClassValue.dexterity)} + {completeNum(allocationValue.dexterity)})</span>
-            <span onClick={onClickDexterityUp}>&gt;</span>
+            <span id="dexterity" onClick={onClickAllocationUp}>&gt;</span>
           </td>
         </tr>
         <tr>
           <td>理力</td>
           <td>
-            <span onClick={onClickIntelligenceDown}>&lt;</span>
+            <span id="intelligence" onClick={onClickAllocationDown}>&lt;</span>
             <span>{completeNum(status.intelligence)}({completeNum(selectedClassValue.intelligence)} + {completeNum(allocationValue.intelligence)})</span>
-            <span onClick={onClickIntelligenceUp}>&gt;</span>
+            <span id="intelligence" onClick={onClickAllocationUp}>&gt;</span>
           </td>
         </tr>
         <tr>
           <td>信仰</td>
           <td>
-            <span onClick={onClickFaithDown}>&lt;</span>
+            <span id="faith" onClick={onClickAllocationDown}>&lt;</span>
             <span>{completeNum(status.faith)}({completeNum(selectedClassValue.faith)} + {completeNum(allocationValue.faith)})</span>
-            <span onClick={onClickFaithUp}>&gt;</span>
+            <span id="faith" onClick={onClickAllocationUp}>&gt;</span>
           </td>
         </tr>
         <tr>
           <td>運</td>
           <td>
-            <span onClick={onClickLuckDown}>&lt;</span>
+            <span id="luck" onClick={onClickAllocationDown}>&lt;</span>
             <span>{completeNum(status.luck)}({completeNum(selectedClassValue.luck)} + {completeNum(allocationValue.luck)})</span>
-            <span onClick={onClickLuckUp}>&gt;</span>
+            <span id="luck" onClick={onClickAllocationUp}>&gt;</span>
           </td>
         </tr>
         <tr>
