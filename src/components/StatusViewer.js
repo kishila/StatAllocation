7c271892eb60907123a871551b06978cae672c9e
kishila <s16g459@stu.kagawa-u.ac.jp>
2017-02-07 22:02:22 +0900

    ステ振りの<tr>でmapメソッド使用に変更

--- src/components/StatusViewer.js
+++ src/components/StatusViewer.js
@@ -3,11 +3,46 @@ import React from 'react';
 // 数字を2桁の文字列に揃える
 const completeNum = (num) => {return (" " + num).slice(-2)}
 
+const paramNames = [
+  {enName: "vigor", jpName: "生命力"},
+  {enName: "attunement", jpName: "集中力"},
+  {enName: "endurance", jpName: "持久力"},
+  {enName: "vitality", jpName: "体力"},
+  {enName: "strength", jpName: "筋力"},
+  {enName: "dexterity", jpName: "技量"},
+  {enName: "intelligence", jpName: "理力"},
+  {enName: "faith", jpName: "信仰"},
+  {enName: "luck", jpName: "運"},
+]
+
 export default function StatusViewer({
   status, selectedClassValue, allocationValue,
   onClickAllocationDown, onClickAllocationUp,
   onClickReset
 }) {
+  const AllocationParam = React.createClass({
+    propTypes: {
+      enName: React.PropTypes.string.isRequired,
+      jpName: React.PropTypes.string.isRequired
+    },
+    render() {
+      return (
+        <tr>
+          <td width="100">{this.props.jpName}</td>
+          <td width="25">{status[this.props.enName]}</td>
+          <td width="70">
+            <span>({completeNum(selectedClassValue[this.props.enName])} + {completeNum(allocationValue[this.props.enName])})</span>
+          </td>
+          <td><span id={this.props.enName} className="glyphicon glyphicon-arrow-down" onClick={onClickAllocationDown}></span></td>
+          <td><span id={this.props.enName} className="glyphicon glyphicon-arrow-up" onClick={onClickAllocationUp}></span></td>
+        </tr>
+      );
+    }
+  });
+  const allocationParames = paramNames.map((param, i) => {
+    return <AllocationParam enName={param.enName} jpName={param.jpName} key={i} />
+  });
+
   return (
     <table>
       <tbody>
@@ -20,87 +55,7 @@ export default function StatusViewer({
             <hr />
           </td>
         </tr>
-        <tr>
-          <td width="100">生命力</td>
-          <td width="25">{status.vigor}</td>
-          <td width="70">
-            <span>({completeNum(selectedClassValue.vigor)} + {completeNum(allocationValue.vigor)})</span>
-          </td>
-          <td><span id="vigor" className="glyphicon glyphicon-arrow-down" onClick={onClickAllocationDown}></span></td>
-          <td><span id="vigor" className="glyphicon glyphicon-arrow-up" onClick={onClickAllocationUp}></span></td>
-        </tr>
-        <tr>
-          <td>集中力</td>
-          <td>{completeNum(status.attunement)}</td>
-          <td>
-            <span>({completeNum(selectedClassValue.attunement)} + {completeNum(allocationValue.attunement)})</span>
-          </td>
-          <td><span id="attunement" className="glyphicon glyphicon-arrow-down" onClick={onClickAllocationDown}></span></td>
-          <td><span id="attunement" className="glyphicon glyphicon-arrow-up" onClick={onClickAllocationUp}></span></td>
-        </tr>
-        <tr>
-          <td>持久力</td>
-          <td>{completeNum(status.endurance)}</td>
-          <td>
-            <span>({completeNum(selectedClassValue.endurance)} + {completeNum(allocationValue.endurance)})</span>
-          </td>
-          <td><span id="endurance" className="glyphicon glyphicon-arrow-down" onClick={onClickAllocationDown}></span></td>
-          <td><span id="endurance" className="glyphicon glyphicon-arrow-up"  onClick={onClickAllocationUp}></span></td>
-        </tr>
-        <tr>
-          <td>体力</td>
-          <td>{completeNum(status.vitality)}</td>
-          <td>
-            <span>({completeNum(selectedClassValue.vitality)} + {completeNum(allocationValue.vitality)})</span>
-          </td>
-          <td><span id="vitality" className="glyphicon glyphicon-arrow-down" onClick={onClickAllocationDown}></span></td>
-          <td><span id="vitality" className="glyphicon glyphicon-arrow-up"  onClick={onClickAllocationUp}></span></td>
-        </tr>
-        <tr>
-          <td>筋力</td>
-          <td>{completeNum(status.strength)}</td>
-          <td>
-            <span>({completeNum(selectedClassValue.strength)} + {completeNum(allocationValue.strength)})</span>
-          </td>
-          <td><span id="strength" className="glyphicon glyphicon-arrow-down" onClick={onClickAllocationDown}></span></td>
-          <td><span id="strength" className="glyphicon glyphicon-arrow-up"  onClick={onClickAllocationUp}></span></td>
-        </tr>
-        <tr>
-          <td>技量</td>
-          <td>{completeNum(status.dexterity)}</td>
-          <td>
-            <span>({completeNum(selectedClassValue.dexterity)} + {completeNum(allocationValue.dexterity)})</span>
-          </td>
-          <td><span id="dexterity" className="glyphicon glyphicon-arrow-down" onClick={onClickAllocationDown}></span></td>
-          <td><span id="dexterity" className="glyphicon glyphicon-arrow-up"  onClick={onClickAllocationUp}></span></td>
-        </tr>
-        <tr>
-          <td>理力</td>
-          <td>{completeNum(status.intelligence)}</td>
-          <td>
-            <span>({completeNum(selectedClassValue.intelligence)} + {completeNum(allocationValue.intelligence)})</span>
-          </td>
-          <td><span id="intelligence" className="glyphicon glyphicon-arrow-down" onClick={onClickAllocationDown}></span></td>
-          <td><span id="intelligence" className="glyphicon glyphicon-arrow-up"  onClick={onClickAllocationUp}></span></td>
-        </tr>
-        <tr>
-          <td>信仰</td>
-          <td>{completeNum(status.faith)}</td>
-          <td>
-            <span>({completeNum(selectedClassValue.faith)} + {completeNum(allocationValue.faith)})</span>
-          </td>
-          <td><span id="faith" className="glyphicon glyphicon-arrow-down" onClick={onClickAllocationDown}></span></td>
-          <td><span id="faith" className="glyphicon glyphicon-arrow-up"  onClick={onClickAllocationUp}></span></td>
-        </tr>
-        <tr>
-          <td>運</td>
-          <td>{completeNum(status.luck)}</td>
-          <td>
-            <span>({completeNum(selectedClassValue.luck)} + {completeNum(allocationValue.luck)})</span>
-          </td>
-          <td><span id="luck" className="glyphicon glyphicon-arrow-down" onClick={onClickAllocationDown}></span></td>
-          <td><span id="luck" className="glyphicon glyphicon-arrow-up"  onClick={onClickAllocationUp}></span></td>
-        </tr>
+          {allocationParames}
         <tr>
           <td colSpan="5">
             <hr />
